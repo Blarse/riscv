b46e1650b81757541eeab287028f25e723ddddde
Egor Ignatov <master.egor.ignatov@gmail.com>
2020-10-30 13:46:51 +0300

    first pass initial parser

--- assembler/common.c
+++ assembler/common.c
@@ -1,8 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <assert.h>
 
-static void panic(char *Message)
+
+#define offsetof(type, member)	__builtin_offsetof (type, member)
+#define MIN(x, y) ((x) <= (y) ? (x) : (y))
+#define MAX(x, y) ((x) >= (y) ? (x) : (y))
+#define CLAMP_MAX(x, max) MIN(x, max)
+#define CLAMP_MIN(x, min) MAX(x, min)
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+
+void panic(const char *Message, ...)
 {
-	puts(Message);
+	va_list Args;
+	va_start(Args, Message);
+	vfprintf(stderr, Message, Args);
+	va_end(Args);
+	fputc('\n', stderr);
 	exit(1);
+}
+
+
+void *xcalloc(size_t num_elems, size_t elem_size) {
+	void *ptr = calloc(num_elems, elem_size);
+	if (!ptr)
+		panic("xcalloc failed");
+	return ptr;
+}
+
+void *xrealloc(void *ptr, size_t num_bytes) {
+	ptr = realloc(ptr, num_bytes);
+	if (!ptr)
+		panic("xrealloc failed");
+	return ptr;
+}
+
+void *xmalloc(size_t num_bytes) {
+	void *ptr = malloc(num_bytes);
+	if (!ptr)
+		panic("xmalloc failed");
+	return ptr;
+}
+
+
+
+
+
+
+
+typedef struct BufHdr {
+	size_t len;
+	size_t cap;
+	char buf[];
+} BufHdr;
+
+#define buf__hdr(b) ((BufHdr *)((char *)(b) - offsetof(BufHdr, buf)))
+
+#define buf_len(b) ((b) ? buf__hdr(b)->len : 0)
+#define buf_cap(b) ((b) ? buf__hdr(b)->cap : 0)
+#define buf_end(b) ((b) + buf_len(b))
+#define buf_sizeof(b) ((b) ? buf_len(b)*sizeof(*b) : 0)
+
+#define buf_free(b) ((b) ? (free(buf__hdr(b)), (b) = NULL) : 0)
+#define buf_fit(b, n) ((n) <= buf_cap(b) ? 0 : ((b) = buf__grow((b), (n), sizeof(*(b)))))
+#define buf_push(b, ...) (buf_fit((b), 1 + buf_len(b)), (b)[buf__hdr(b)->len++] = (__VA_ARGS__))
+#define buf_printf(b, ...) ((b) = buf__printf((b), __VA_ARGS__))
+#define buf_clear(b) ((b) ? buf__hdr(b)->len = 0 : 0)
+
+void *buf__grow(const void *buf, size_t new_len, size_t elem_size) {
+	assert(buf_cap(buf) <= (SIZE_MAX - 1)/2);
+	size_t new_cap = CLAMP_MIN(2*buf_cap(buf), MAX(new_len, 16));
+	assert(new_len <= new_cap);
+	assert(new_cap <= (SIZE_MAX - offsetof(BufHdr, buf))/elem_size);
+	size_t new_size = offsetof(BufHdr, buf) + new_cap*elem_size;
+	BufHdr *new_hdr;
+	if (buf) {
+		new_hdr = xrealloc(buf__hdr(buf), new_size);
+	} else {
+		new_hdr = xmalloc(new_size);
+		new_hdr->len = 0;
+	}
+	new_hdr->cap = new_cap;
+	return new_hdr->buf;
 }
--- assembler/main.c
+++ assembler/main.c
@@ -1,18 +1,98 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "common.c"
 
+#define LINE_BUFFER_SIZE 256
+
 typedef struct
+{
+	uint32 Value;
+	char Name[32];
+} symbol;
+
+int isempty(const char *String)
+{
+	while(*String != '\0')
+	{
+		if(!isspace(*String))
+			return 0;
+		String++;
+	}
+	return 1;
+}
+
+
+char * firstchar(const char *String)
+{
+	while(*String != '\0')
+	{
+		if(!isspace(*String))
+			return String;
+		String++;
+	}
+	return NULL;
+}
 
 void pass1(FILE* InputFile)
 {
+	char LineBuffer[LINE_BUFFER_SIZE];
+	char *LinePtr;
+	uint32 LineNum = 0;
+	size_t LineLength = 0;
+	uint32 Result = 0;
 
+
+	uint32 LocationCounter;
+	char *Lable, *Mnemonic, *Operands, *Comment;
+
+
+	for(;;)
+	{
+		++LineNum;
+
+		LinePtr = fgets(LineBuffer, LINE_BUFFER_SIZE, InputFile);
+		if(!LinePtr)
+		{
+			if(feof(InputFile))
+				break;
+			panic("[%s] file parsing error",__func__);
+		}
+
+		if(isempty(LineBuffer))
+			continue;
+
+		LineLength = strlen(LineBuffer);
+
+		if(LineBuffer[LineLength-1] != '\n')
+			panic("The line %d is too long!", LineNum);
+
+		LineBuffer[LineLength-1] = '\0';
+
+
+
+		LinePtr = firstchar(LineBuffer);
+		if(*LinePtr == '.')
+		{
+			//directive
+
+		}
+		else
+		{
+			//check for a lable
+		}
+
+
+		printf("%d: '%s'\n", LineNum, LineBuffer);
+	}
 
 }
 
 
 int main(int ArgsCount, char **Args)
 {
+
 	if(ArgsCount != 3)
 		panic("Wrong arguments\nUsage: %s <input-file> <output-file>", Args[0]);
 
